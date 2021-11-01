2dad6cdb8b602ce47458797d319f19f5f7c35fde
annt <1112dara@gmail.com>
2021-11-01 16:42:10 +0800

    GET&POST

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action='api/get/book/1' method='POST'>身高
+    <input type='hight' name='hight' value=''>cm
+    <br>
+    體重<input type='weight' name='weight' value=''>kg<br>
+    <input type='submit' value='開始計算'>
+</form>
+
+</body>
+</html>
